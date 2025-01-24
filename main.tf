535ccb41c03360f5c447aca42522864a57eb5038
ankushgithub1 <102030878+ankushgithub1@users.noreply.github.com>
2025-01-24 14:39:51 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.aws_region
+  profile = "default"
+}
+
+# Security Group for HTTP and SSH Access
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# EC2 Instance with User Data for Nginx
+resource "aws_instance" "web_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "nginx-web-server"
+  }
+}
+
